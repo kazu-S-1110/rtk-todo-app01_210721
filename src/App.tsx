558f3f9fb8d8b3f0ee21cf0fbd5449797fd5632c
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-21 13:00:48 +0900

    styleとの戦い。便利だけどまだまだ慣れない、、、

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import styles from './App.module.scss';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Box } from '@chakra-ui/react';
+import { ChakraProvider, Center } from '@chakra-ui/react';
 import Header from './components/header/Header';
 
 const App: React.VFC = () => {
   return (
     <ChakraProvider>
-      <Box bg="tomato" w="100%" p={4} color="white">
-        This is the Box
-      </Box>
-      <Header />
+      <div className={styles.root}>
+        <div className={styles.wrapper}>
+          <Header />
+        </div>
+      </div>
     </ChakraProvider>
   );
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,23 @@
-import { Center } from '@chakra-ui/react';
+import { Center, Text } from '@chakra-ui/react';
 
 const Header = () => {
   return (
     <div>
-      <Center bg="Highlight" h="60px" color="white">
-        This is the Center
+      <Center
+        bg="#8EC5FC"
+        w="70vw"
+        h="70vh"
+        bgGradient="linear(to-l, #8EC5FC, #fdfdfd)"
+        borderRadius="10"
+      >
+        <Text
+          bgGradient="linear(to-l, #7928CA,#FF0080)"
+          bgClip="text"
+          fontSize="6xl"
+          fontWeight="extrabold"
+        >
+          Redux Toolkit Todo
+        </Text>
       </Center>
     </div>
   );
