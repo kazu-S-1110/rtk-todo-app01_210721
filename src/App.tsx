c69cdb82a4a0bc80aeee825588f7c9859e4f202d
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-21 12:37:55 +0900

    Headerコンポーネント作成。ファイル整理。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import styles from './App.module.scss';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import Header from './components/header/Header';
 
-function App() {
-  return <ChakraProvider></ChakraProvider>;
-}
+const App: React.VFC = () => {
+  return (
+    <ChakraProvider>
+      <Box bg="tomato" w="100%" p={4} color="white">
+        This is the Box
+      </Box>
+      <Header />
+    </ChakraProvider>
+  );
+};
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import { Center } from '@chakra-ui/react';
+
+const Header = () => {
+  return (
+    <div>
+      <Center bg="Highlight" h="60px" color="white">
+        This is the Center
+      </Center>
+    </div>
+  );
+};
+
+export default Header;
